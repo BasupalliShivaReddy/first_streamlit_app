9a8be4737ae54fb122ac4ad304f759df5f397d15
BasupalliShivaReddy <127378750+BasupalliShivaReddy@users.noreply.github.com>
2023-03-09 11:54:00 +0530

    Update streamlit_app.py
    Edited header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('New Healthy Breakfast')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 abd Blueberry otmeal')
-streamlit.text('Egg omlet and Mango Smoothie')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
